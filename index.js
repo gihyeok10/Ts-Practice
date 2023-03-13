8073daf91e1f6a18cad32fefdcc82bf08831fea7
gihyeok Kim <gh9812301@naver.com>
2023-03-13 16:26:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var members = ["kim", "park"];
+var membersNumber = [15, 19];
+var meem = {
+    member: "kim",
+    membaer2: "park",
+};
+var userName = "김기혁";
+var userAge = 13;
+var userArea = "대전";
+var like = {
+    songName: "아이야",
+    singer: "휘재",
+};
+var projects = {
+    members: ["kim", "pakr"],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,22 @@
+let members: string[] = ["kim", "park"];
+let membersNumber: number[] = [15, 19];
+
+let meem: { member: string; membaer2: string } = {
+  member: "kim",
+  membaer2: "park",
+};
+
+const userName: string = "김기혁";
+const userAge: number = 13;
+const userArea: string = "대전";
+
+const like: { songName: string; singer: string } = {
+  songName: "아이야",
+  singer: "휘재",
+};
+
+let projects: { members: string[]; days: number; started: boolean } = {
+  members: ["kim", "pakr"],
+  days: 30,
+  started: true,
+};
