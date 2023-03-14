37c7e989ef8398d2995dbdb5601e0adf68baf86f
gihyeok Kim <gh9812301@naver.com>
2023-03-14 10:05:03 +0900

    readonly&typealias

--- typeFile/function.js
+++ typeFile/function.js
@@ -12,11 +12,6 @@ function 하수(x) {
 //void => return쓰기 싫은 함수에 쓰여짐 return 못함 텅 비었다?
 // x? x가 들어올수도 안들어올수도 있다.  number|undefined 랑 똑같다 하나의 유니온 타입
 함수2(2);
-// function 마지막(x:number |string):void{
-//     if(네로잉?){
-//     console.log(x+3)
-//     }
-// }
 function 안녕하세요(x) {
     if (x) {
         console.log("안녕하세요" + x);
@@ -48,3 +43,72 @@ function 결혼확률(pay, house, atra) {
     }
 }
 console.log(결혼확률(100, true, "상"));
+// 네로잉 함수
+// 타입을 하나로 거른다.
+function 마지막(x) {
+    if (typeof x === "string" || typeof x === "number") {
+        return x + "1";
+    }
+    else {
+        return x + 1;
+    }
+}
+function 내함수(x) {
+    var array = [];
+    if (typeof x === "number") {
+        array[0] = x;
+    }
+}
+function 내함수2(x) {
+    var array = [];
+    array[0] = x;
+}
+//assertion 문법의 용도 => 여러개 중에 하나로 확정할때 쓰는거. && 무슨타입이 뭔지 확실히 알때만 쓰기
+// assertion 타입 덮어 씌우기
+function clean(x) {
+    var finish = [];
+    x.forEach(function (item) {
+        if (typeof item === "string") {
+            finish.push(parseFloat(item));
+        }
+        else {
+            finish.push(item);
+        }
+    });
+    return finish;
+}
+console.log(clean([123, "22", 123, "521"]));
+function 과목출력(x) {
+    if (Array.isArray(x.subject) === true) {
+        var le = 0;
+        le = x.subject.length - 1;
+        console.log("이사람의 마지막 과목은??:", x.subject[le]);
+    }
+    else {
+        console.log("이사람은 하나의 과목:", x.subject);
+    }
+}
+과목출력({ subject: ["과학", "수학", "물리"] });
+var arr = { lll: ["아아", "메메"] };
+console.log(arr.lll.length);
+var 동물 = "kim";
+var 동물2 = { name: "kim", age: 20 };
+// 오브젝트 안에 자료는 자유롭게 수정이 가능하다.
+var 출생지역 = { region: "seoul" };
+출생지역.region = "daegu";
+//? option string|undefined
+var 여친 = {
+    name: "엠버",
+};
+// => {x:number, y:number}
+var position = { x: 10, y: 20 };
+var 테스트용변수 = {
+    size: 123,
+    position: [1, 2, 4, 5],
+};
+var inww = {
+    name: "김기혁",
+    email: "gh9812301",
+    phone: 123123,
+    adult: true,
+};
--- typeFile/function.ts
+++ typeFile/function.ts
@@ -15,12 +15,6 @@ function 하수(x?: number): void {
 // x? x가 들어올수도 안들어올수도 있다.  number|undefined 랑 똑같다 하나의 유니온 타입
 함수2(2);
 
-// function 마지막(x:number |string):void{
-//     if(네로잉?){
-//     console.log(x+3)
-//     }
-// }
-
 function 안녕하세요(x?: string): void {
   if (x) {
     console.log("안녕하세요" + x);
@@ -54,3 +48,119 @@ function 결혼확률(pay: number, house: boolean, atra: string): string | void 
 }
 
 console.log(결혼확률(100, true, "상"));
+
+// 네로잉 함수
+// 타입을 하나로 거른다.
+function 마지막(x: number | string) {
+  if (typeof x === "string" || typeof x === "number") {
+    return x + "1";
+  } else {
+    return x + 1;
+  }
+}
+
+function 내함수(x: number | string) {
+  let array: number[] = [];
+  if (typeof x === "number") {
+    array[0] = x;
+  }
+}
+
+function 내함수2(x: number | string) {
+  let array: number[] = [];
+  array[0] = x as number;
+}
+
+//assertion 문법의 용도 => 여러개 중에 하나로 확정할때 쓰는거. && 무슨타입이 뭔지 확실히 알때만 쓰기
+// assertion 타입 덮어 씌우기
+
+function clean(x: (number | string)[]) {
+  let finish: number[] = [];
+
+  x.forEach((item) => {
+    if (typeof item === "string") {
+      finish.push(parseFloat(item));
+    } else {
+      finish.push(item);
+    }
+  });
+  return finish;
+}
+
+console.log(clean([123, "22", 123, "521"]));
+
+function 과목출력(x: { subject: string | string[] }) {
+  if (Array.isArray(x.subject) === true) {
+    let le = 0;
+    le = x.subject.length - 1;
+    console.log("이사람의 마지막 과목은??:", x.subject[le]);
+  } else {
+    console.log("이사람은 하나의 과목:", x.subject);
+  }
+}
+
+과목출력({ subject: ["과학", "수학", "물리"] });
+
+const arr = { lll: ["아아", "메메"] };
+console.log(arr.lll.length);
+
+// 변수에 타입 지정 (타입 에일리어스 => 별칭)
+// type 작명할때 영어 대문자로 국룰 && 뒤에 type정도?
+type Animal = string | number | undefined;
+let 동물: Animal = "kim";
+type Animal2 = { name: string; age: number };
+let 동물2: Animal2 = { name: "kim", age: 20 };
+
+// 오브젝트 안에 자료는 자유롭게 수정이 가능하다.
+const 출생지역 = { region: "seoul" };
+출생지역.region = "daegu";
+
+//여친 오브젝트 수정을 막으려면?
+type Girlfriend = {
+  readonly name?: string;
+};
+//? option string|undefined
+
+const 여친: Girlfriend = {
+  name: "엠버",
+};
+
+//readonly => 읽기 전용 수정 불가 but js파일에는 실행됌
+
+// 타입 합치기
+type Name = string;
+type Age = number;
+type Person = Name | Age;
+
+type PositionX = { x: number };
+type PositionY = { y: number };
+//같은 이름의 type 변수 재정의 불가능
+type NewType = PositionX & PositionY;
+// => {x:number, y:number}
+
+let position: NewType = { x: 10, y: 20 };
+
+type AS = { x: number; y: string };
+type Aw = { x: number; y: string };
+
+type Plus = AS & Aw;
+
+type Obj = { color?: string; size: number; readonly position: number[] };
+
+let 테스트용변수: Obj = {
+  size: 123,
+  position: [1, 2, 4, 5],
+};
+
+type Infor = { name: string; phone: number; email: string };
+
+type Adult = { adult: boolean };
+
+type Plus2 = Infor & Adult;
+
+let inww: Plus2 = {
+  name: "김기혁",
+  email: "gh9812301",
+  phone: 123123,
+  adult: true,
+};
