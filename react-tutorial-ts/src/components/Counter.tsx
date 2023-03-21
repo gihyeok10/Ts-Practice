8a2f4d24ece58dbec89743d7702e63655929ea20
gihyeok Kim <gh9812301@naver.com>
2023-03-21 13:00:35 +0900

    reducer reset

--- react-tutorial-ts/src/components/Counter.tsx
+++ react-tutorial-ts/src/components/Counter.tsx
@@ -3,10 +3,17 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: "increment" | "decrement";
   payload: number;
 };
+
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
+
 const initialState = { count: 0 };
 
 function reducer(state: CounterState, action: CounterAction) {
@@ -14,8 +21,11 @@ function reducer(state: CounterState, action: CounterAction) {
     case "increment":
       return { count: state.count + action.payload };
 
-    case "descrement":
+    case "decrement":
       return { count: state.count - action.payload };
+
+    case "reset":
+      return initialState;
     default:
       return state;
   }
@@ -32,6 +42,7 @@ export const Counter = () => {
       <button onClick={() => disptch({ type: "decrement", payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => disptch({ type: "reset" })}>Reset</button>
     </>
   );
 };
